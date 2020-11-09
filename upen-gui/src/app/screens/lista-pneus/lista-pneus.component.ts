b5c1daac11271a943a4004effa44c7726563a27f
João V. Valadares <jvvm@cin.ufpe.br>
2020-11-09 18:05:08 -0300

    feat(ListaPneu)Estrutura da página de lista de pneu com algumas variáveis definidas

--- upen-gui/src/app/screens/lista-pneus/lista-pneus.component.ts
+++ upen-gui/src/app/screens/lista-pneus/lista-pneus.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+
+import { Observable } from 'rxjs'
+import { Pneu } from '../../../../../common/pneu'
+import { ListaPneusService } from 'src/app/services/ListaPneuService/lista-pneus.service'
 
 @Component({
   selector: 'app-lista-pneus',
@@ -7,7 +12,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaPneusComponent implements OnInit {
 
-  constructor() { }
+  pneu: Pneu = new Pneu();
+  pneus: Pneu[] = [];
+  atribuicao: [string, string, string, boolean] = ["", "", "", false];
+  id: string;
+  marca: string;
+  data: string;
+
+  constructor(private route: ActivatedRoute, private ListaPneusService: ListaPneusService) { }
 
   ngOnInit(): void {
   }
