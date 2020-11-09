d8717aafad32a48689b28b9e44c200d80bc5116f
João V. Valadares <jvvm@cin.ufpe.br>
2020-11-09 19:30:04 -0300

    feat(ListaPneu)Feito cenário de cadastro de pneu

--- upen-gui/src/app/screens/lista-pneus/lista-pneus.component.ts
+++ upen-gui/src/app/screens/lista-pneus/lista-pneus.component.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs'
 import { Pneu } from '../../../../../common/pneu'
 import { ListaPneusService } from 'src/app/services/ListaPneuService/lista-pneus.service'
 
+
 @Component({
   selector: 'app-lista-pneus',
   templateUrl: './lista-pneus.component.html',
@@ -18,10 +19,86 @@ export class ListaPneusComponent implements OnInit {
   id: string;
   marca: string;
   data: string;
+  clickModal = -1;
+  idduplicado: boolean = false;
+  popupCadastro: boolean = false;
+  botaoCadastrarPressionado: boolean;
 
-  constructor(private route: ActivatedRoute, private ListaPneusService: ListaPneusService) { }
+  modal = document.getElementById("myModal");
+  btn = document.getElementById("myBtn");
+  span = document.getElementById("mySpan");
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private ListaPneusService: ListaPneusService) {}
+
+  public toggleField() {
+    this.clickModal = this.clickModal * (-1);
   }
 
+  adicionarPneu(pneu: Pneu): void {
+    this.botaoCadastrarPressionado = true;
+
+    if(!this.checkAllFilled()){
+      return;
+    }
+
+    this.ListaPneusService.criar(pneu)
+      .subscribe(
+        ar => {
+          if (ar) {
+            this.pneus.push(ar);
+            this.pneu = new Pneu();
+            alert("Pneu cadastrado");
+            this.botaoCadastrarPressionado = false;
+            this.fecharPopup();
+          } else {
+            this.idduplicado =  true;
+          }
+        },
+        msg => { alert (msg.message); }
+      );
+  }
+
+  checkAllFilled(): boolean{
+    return (!this.checkStringNotFilled(this.pneu.id) && !this.checkStringNotFilled(this.pneu.marca) &&
+      !this.checkNumberNotFilled(this.pneu.aro) && !this.checkNumberNotFilled(this.pneu.largura)
+      && !this.checkNumberNotFilled(this.pneu.altura)) && !this.checkNumberNotFilled(this.pneu.capacidade) && 
+      !this.checkNumberNotFilled(this.pneu.kmh) && !this.checkNumberNotFilled(this.pneu.treadwear) && !this.checkStringNotFilled(this.pneu.data);
+  }
+
+  checkStringNotFilled( s: string): boolean{
+    return (s.trim() == "")
+  }
+
+  checkNumberNotFilled(n: number) : boolean {
+    return(n == 0);
+  }
+
+  resetidDuplicado() {
+    this.idduplicado = false;
+  }
+
+  abrirPopup(): void{
+    this.popupCadastro = true;
+  }
+
+  fecharPopup(): void{
+    this.popupCadastro = false;
+  }
+
+  onMove(): void {
+    this.idduplicado = false;
+  }
+
+  ngOnInit() :void {
+    this.popupCadastro = false;
+    this.botaoCadastrarPressionado = false;
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+    });
+    this.ListaPneusService.getPneus()
+      .subscribe(
+        as => { this.pneus = as; },
+        msg => { alert(msg.message); }
+      )
+  }
 }
--- upen-gui/src/app/services/ListaPneuService/lista-pneus.service.ts
+++ upen-gui/src/app/services/ListaPneuService/lista-pneus.service.ts
@@ -14,6 +14,27 @@ export class ListaPneusService {
     private servURL = 'http://localhost:3000';
 
     constructor(private http: HttpClient) {}
+    
+    criar(pneu: Pneu): Observable<any> {
+      return this.http.post<any>(this.servURL + "/pneu", pneu, {headers: this.headers})
+        .pipe(
+          retry(2),
+          map(res => {if (res.success) {return pneu;} else {return null;}})
+        );
+    }
 
+    getPneu(id: string): Observable<Pneu> {
+      return this.http.get<Pneu>(this.servURL + "/pneus/" + id)
+        .pipe(
+          retry(2)
+        );
+    }
+    
+    getPneus(): Observable<Pneu[]> {
+      return this.http.get<Pneu[]>(this.servURL + "/pneus/")
+        .pipe(
+          retry(2)
+        );
+    }
     
 }
