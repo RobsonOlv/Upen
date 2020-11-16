4a3d9b094f2f44c5057eafd757508698a383215c
afss2 <afss2@cin.ufpe.br>
2020-11-16 18:01:26 -0300

    (featFuncionario) Refatoração dos testes de aceitação

--- tests-acceptance/stepdefinitions/funcionario.ts
+++ tests-acceptance/stepdefinitions/funcionario.ts
@@ -12,6 +12,49 @@ let samePlate = ((elem, plate) => elem.element(by.name('placalist')).getText().t
 
 let pAND = ((p,q,r,s) => p.then(a => q.then(b => r.then(c => s.then(d => (a && b && c && d))))))
 
+async function cadastrarFunc(name,cpf,ffunction,telephone) {
+        await element(by.name('dialogcadastrobutton')).click();
+        await $("input[name='nomebox']").sendKeys(<string> name);
+        await $("input[name='cpfbox']").sendKeys(<string> cpf);
+        await $("input[name='funcaobox']").sendKeys(<string> ffunction);
+        await $("input[name='telefonebox']").sendKeys(<string> telephone);
+        await element(by.name('cadastrarbutton')).click();
+}
+
+async function assertEqualSize(set,n) {
+    await set.then(elems => expect (Promise.resolve(elems.length)).to.eventually.equal(n));
+    
+}
+
+async function assertElementsWithSameParameters(name,cpf,ffunction, telephone,n) {
+    var allemployees: ElementArrayFinder = element.all(by.name('funclist'));
+    var sameemployee = allemployees.filter(elem => pAND(sameName(elem,name),sameCPF(elem,cpf),sameFunction (elem,ffunction),samePhone(elem,telephone)))
+    await assertEqualSize(sameemployee,n)
+}
+
+async function assertElementsWithSameCPF(cpf,n) {
+    var allcpfs: ElementArrayFinder = element.all(by.name('cpflist'));
+    var samecpfs = allcpfs.filter( elem => sameCPF(elem,cpf));
+    assertEqualSize (samecpfs,n)
+}
+
+
+async function clickOnSameCPF(cpf, button) {
+    var allcpfs: ElementArrayFinder = element.all(by.name('funclist'));
+    var samecpf = allcpfs.filter( elem => sameCPF(elem,cpf));
+    await samecpf.all(by.name(button)).click();
+    
+}
+
+async function clickOnSamePlate(plate,button) {
+    var allplates: ElementArrayFinder = element.all(by.name('listaVeic'));
+    var sameplate = allplates.filter(elem => samePlate(elem,plate));
+    await sameplate.all(by.name(button)).click();
+    await element.all(by.name('finalizarbutton')).click();
+    
+}
+
+
 defineSupportCode(function ({ Given, When, Then}) {
     Given(/^I am at the employees page$/, async () => {
         await browser.get("http://localhost:4200/");
@@ -21,39 +64,22 @@ defineSupportCode(function ({ Given, When, Then}) {
     });
 
     Given(/^I cannot see an employee with CPF "(\d*)" in the employees list$/, async(cpf) => {
-        var allcpfs: ElementArrayFinder = element.all(by.name('cpflist'));
-        var samecpfs = allcpfs.filter( elem => sameCPF(elem,cpf));
-        await samecpfs.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+        await assertElementsWithSameCPF(cpf,0);
 
     });
 
-    When(/^I try to register the employee "([^\"]*)" with CPF "(\d*)", function "([^\"]*)" and phone number "(\d*)"$/, async (name,cpf,efunction,pnumber) => {
-        await element(by.name('dialogcadastrobutton')).click();
-        await $("input[name='nomebox']").sendKeys(<string> name);
-        await $("input[name='cpfbox']").sendKeys(<string> cpf);
-        await $("input[name='funcaobox']").sendKeys(<string> efunction);
-        await $("input[name='telefonebox']").sendKeys(<string> pnumber);
-        await element(by.name('cadastrarbutton')).click();
+    When(/^I try to register the employee "([^\"]*)" with CPF "(\d*)", function "([^\"]*)" and phone number "(\d*)"$/, async (name,cpf,ffunction,telephone) => {
+        await cadastrarFunc(name,cpf,ffunction,telephone);
 
     });
 
-    Then(/^I can see "([^\"]*)" with CPF "(\d*)", function "([^\"]*)" and phone number "(\d*)" in the employees list$/, async (name,cpf,efunction,pnumber) => {
-        var allemployees: ElementArrayFinder = element.all(by.name('funclist'));
-        await allemployees.filter(elem => pAND(sameName(elem,name),sameCPF(elem,cpf),sameFunction (elem,efunction),samePhone(elem,pnumber))).then
-                (elems => expect (Promise.resolve(elems.length)).to.eventually.equal(1));
-
+    Then(/^I can see "([^\"]*)" with CPF "(\d*)", function "([^\"]*)" and phone number "(\d*)" in the employees list$/, async (name,cpf,ffunction,telephone) => {
+        await assertElementsWithSameParameters(name,cpf,ffunction,telephone,1);
     });
 
     Given(/^I can see an employee with CPF "(\d*)" in the employees list$/, async(cpf) => {
-        await element(by.name('dialogcadastrobutton')).click();
-        await $("input[name='nomebox']").sendKeys(<string> "Junior");
-        await $("input[name='cpfbox']").sendKeys(<string> cpf);
-        await $("input[name='funcaobox']").sendKeys(<string> "Mecanico");
-        await $("input[name='telefonebox']").sendKeys(<string> "81988751222");
-        await element(by.name('cadastrarbutton')).click();
-        var allemployees: ElementArrayFinder = element.all(by.name('funclist'));
-        await allemployees.filter(elem => pAND(sameName(elem,"Junior"),sameCPF(elem,cpf),sameFunction (elem,"Mecanico"),samePhone(elem,"81988751222"))).then
-                (elems => expect (Promise.resolve(elems.length)).to.eventually.equal(1));
+        await cadastrarFunc("Junior",cpf,"Mecanico","81988751222");
+        await assertElementsWithSameParameters("Junior",cpf,"Mecanico","81988751222", 1)
     });
 
     When(/^I try to remove the employee with CPF "(\d*)"$/, async(cpf) => {
@@ -65,55 +91,32 @@ defineSupportCode(function ({ Given, When, Then}) {
         
     });
 
-    Then(/^I cannot see "([^\"]*)" with CPF "(\d*)", function "([^\"]*)" and phone number "(\d*)" in the employees list$/, async(name,cpf,efunction,pnumber) => {
-        var allemployees: ElementArrayFinder = element.all(by.name('funclist'));
-        await allemployees.filter(elem => pAND(sameName(elem,name),sameCPF(elem,cpf),sameFunction (elem,efunction),samePhone(elem,pnumber))).then
-                (elems => expect (Promise.resolve(elems.length)).to.eventually.equal(0));
-
+    Then(/^I cannot see "([^\"]*)" with CPF "(\d*)", function "([^\"]*)" and phone number "(\d*)" in the employees list$/, async(name,cpf,ffunction,telephone) => {
+        await assertElementsWithSameParameters(name,cpf,ffunction,telephone,0)
     });
 
     When(/^I try to assign a vehicle with plate "([^\"]*)" to the employee with CPF "(\d*)"$/, async(plate,cpf) => {
-        var allcpfs: ElementArrayFinder = element.all(by.name('funclist'));
-        var samecpf = allcpfs.filter( elem => sameCPF(elem,cpf));
-        await samecpf.all(by.name('openatribbutton')).click();
-        var allplates: ElementArrayFinder = element.all(by.name('listaVeic'));
-        var sameplate = allplates.filter(elem => samePlate(elem,plate));
-        await sameplate.all(by.name('atribuirbutton')).click();
-        await element.all(by.name('finalizarbutton')).click();
+        await clickOnSameCPF(cpf,'openatribbutton')
+        await clickOnSamePlate (plate, 'atribuirbutton')
     });
 
     Then(/^I can see the the plate "([^\"]*)" in the employee with CPF "(\d*)" vehicles list$/, async(plate,cpf) => {
         await browser.refresh()
         await browser.sleep(1000)
-        var allcpfs: ElementArrayFinder = element.all(by.name('funclist'));
-        var samecpf = allcpfs.filter( elem => sameCPF(elem,cpf));
-        await samecpf.all(by.name('vehiclelist')).click();
+        await clickOnSameCPF(cpf,'vehiclelist')
         var allplates = element(by.cssContainingText('mat-option .mat-option-text', <string> plate));
         await expect (allplates.getText()).to.eventually.equal(<string> plate)
         
     });
 
+
     Given(/^I can see an employee with CPF "(\d*)" assigned with "([^\"]*)" in the employees list$/, async (cpf,plate) => {
-        await element(by.name('dialogcadastrobutton')).click();
-        await $("input[name='nomebox']").sendKeys(<string> "Antonio");
-        await $("input[name='cpfbox']").sendKeys(<string> cpf);
-        await $("input[name='funcaobox']").sendKeys(<string> "Vistoria");
-        await $("input[name='telefonebox']").sendKeys(<string> "81993751222");
-        await element(by.name('cadastrarbutton')).click();
-        var allemployees: ElementArrayFinder = element.all(by.name('funclist'));
-        await allemployees.filter(elem => pAND(sameName(elem,"Antonio"),sameCPF(elem,cpf),sameFunction (elem,"Vistoria"),samePhone(elem,"81993751222"))).then
-                (elems => expect (Promise.resolve(elems.length)).to.eventually.equal(1));
-        var allcpfs: ElementArrayFinder = element.all(by.name('funclist'));
-        var samecpf = allcpfs.filter( elem => sameCPF(elem,cpf));
-        await samecpf.all(by.name('openatribbutton')).click();
-        var allplates: ElementArrayFinder = element.all(by.name('listaVeic'));
-        var sameplate = allplates.filter(elem => samePlate(elem,plate));
-        await sameplate.all(by.name('atribuirbutton')).click();
-        await element.all(by.name('finalizarbutton')).click();
+        await cadastrarFunc("Antonio", cpf, "Vistoria","81993751222")
+        await assertElementsWithSameParameters("Antonio", cpf, "Vistoria", "81993751222", 1)
+        await clickOnSameCPF(cpf,'openatribbutton')
+        await clickOnSamePlate(plate,'atribuirbutton')
         await browser.refresh()
-        var searchcpf: ElementArrayFinder = element.all(by.name('funclist'));
-        var samecpffunc = searchcpf.filter( elem => sameCPF(elem,cpf));
-        await samecpffunc.all(by.name('vehiclelist')).click();
+        await clickOnSameCPF(cpf,'vehiclelist')
         var allplatesfunc = element(by.cssContainingText('mat-option .mat-option-text', <string> plate));
         await expect (allplatesfunc.getText()).to.eventually.equal(<string> plate)
         
@@ -121,28 +124,17 @@ defineSupportCode(function ({ Given, When, Then}) {
 
     When(/^I try to unassign the vehicle with plate "([^\"]*)" from the employee with CPF "(\d*)"$/, async (plate,cpf) => {
         await browser.refresh()
-        var allcpfs: ElementArrayFinder = element.all(by.name('funclist'));
-        var samecpf = allcpfs.filter( elem => sameCPF(elem,cpf));
-        await samecpf.all(by.name('openatribbutton')).click();
-        var allplates: ElementArrayFinder = element.all(by.name('listaVeic'));
-        var sameplate = allplates.filter(elem => samePlate(elem,plate));
-        await sameplate.all(by.name('desatribuirbutton')).click();
-        await element.all(by.name('finalizarbutton')).click();
+        await clickOnSameCPF(cpf,'openatribbutton')
+        await clickOnSamePlate(plate,'desatribuirbutton')
     });
 
     Then(/^I cannot see the vehicle plate "([^\"]*)" in the employee with CPF "(\d*)" vehicles list$/, async (plate,cpf) => {
         await browser.refresh()
         await browser.sleep(1000)
-        var allcpfs: ElementArrayFinder = element.all(by.name('funclist'));
-        var samecpf = allcpfs.filter( elem => sameCPF(elem,cpf));
-        await samecpf.all(by.name('vehiclelist')).click();
+        await clickOnSameCPF(cpf,'vehiclelist')
         var allplates = element(by.cssContainingText('mat-option .mat-option-text', "Nenhum"));
         await expect (allplates.getText()).to.eventually.equal("Nenhum")
 
     });
-
-
-
-
 
 })
